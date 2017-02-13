6af2e1294d3d1003194364faaf390596c6a267b8
mapaux <josevi.agullo@gmail.com>
2017-02-13 17:46:55 +0000

    Adding methods to create an empty index

---
+++ ElasticsearchIndexer.ApplicationServices/IndexerHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace ElasticsearchIndexer.ApplicationServices
+{
+    public class IndexerHelper
+    {
+        public bool CreateNewIndex(string newIndexName)
+        {
+            var indexExists = IndexExists(newIndexName);
+
+            if (indexExists)
+            {
+                DeleteExistingIndex(newIndexName);
+            }
+
+            CreateNewIndex(newIndexName);
+
+            return IndexExists(newIndexName);
+        }
+
+        private void DeleteExistingIndex(string newIndexName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool IndexExists(string newIndexName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ElasticsearchIndexer.ApplicationServices/IndexerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace ElasticsearchIndexer.ApplicationServices
+{
+    public class IndexerService
+    {
+        public IndexerService()
+        {
+            
+        }
+
+        public async Task CreateIndex(DateTime dateTime)
+        {
+            var newIndexName = $"elasticIndexer-{dateTime.ToString("yyyy-MM-dd-HH-mm")}".ToLower(CultureInfo.InvariantCulture);
+
+            var indexerHelper = new IndexerHelper();
+
+            indexerHelper.CreateNewIndex(newIndexName);
+        }
+    }
+}
