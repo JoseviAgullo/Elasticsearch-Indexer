8d143a4cbccf88bd21bdb705d51c257827dd0f14
mapaux <josevi.agullo@gmail.com>
2017-02-13 17:35:19 +0000

    Added Nest and model

---
+++ ElasticsearchIndexer.Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ElasticsearchIndexer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> Keywords { get; set; }
+
+        public double Price { get; set; }
+    }
+}
